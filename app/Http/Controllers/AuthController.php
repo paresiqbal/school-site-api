d55104bec58c93a7ebf25a8e98da3b164e41c70c
paresiqbal <thecrowdel@gmail.com>
2024-09-21 13:40:10 +0700

    Add AuthController register method to handle user registration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        return 'register';
+        $fields = $request->validate([
+            'username' => 'required|unique:users,username|max:255', // Specify the table and column
+            'password' => 'required',
+        ]);
+
+
+        $user = User::create($fields);
+
+        $token = $user->createToken($request->username);
+
+        return [
+            'username' => $user,
+            'token' => $token->plainTextToken
+        ];
     }
 
     public function login(Request $request)
---
+++ app/Models/User.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Model
+{
+    use HasFactory, Notifiable, HasApiTokens;
+
+    protected $fillable = [
+        'username',
+        'password',
+    ];
+}
