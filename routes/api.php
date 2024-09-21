85774ef95f03295045395825a37277c0535fa402
paresiqbal <thecrowdel@gmail.com>
2024-09-21 13:55:43 +0700

    Refactor AuthController logout route to add authentication middleware

--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,4 @@ Route::apiResource('news', NewsController::class);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
