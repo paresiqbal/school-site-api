b7ea5bc3089bd7d6491a7e798d12418fdad13d20
paresiqbal <thecrowdel@gmail.com>
2024-09-21 15:09:02 +0700

    Update

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\NewsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::apiResource('news', NewsController::class);
 
