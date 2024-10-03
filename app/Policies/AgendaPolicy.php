87090d6816554ba6858a8b10382ddb3bfad55188
paresiqbal <thecrowdel@gmail.com>
2024-10-03 14:05:27 +0700

    Update

--- app/Policies/AgendaPolicy.php
+++ app/Policies/AgendaPolicy.php
@@ -8,59 +8,10 @@ use Illuminate\Auth\Access\Response;
 
 class AgendaPolicy
 {
-    /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
+    public function modified(User $user, Agenda $agenda): Response
     {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Agenda $agenda): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function update(User $user, Agenda $agenda): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(User $user, Agenda $agenda): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Agenda $agenda): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Agenda $agenda): bool
-    {
-        //
+        return $user->id === $agenda->user_id
+            ? Response::allow()
+            : Response::deny('You do not own this news');
     }
 }
