359c1be49905a27a9cff2df3f6bdd7f885d5a465
paresiqbal <thecrowdel@gmail.com>
2024-10-03 13:25:49 +0700

    Refactor Agenda migration to add title, description, date, and user_id columns

--- database/migrations/2024_10_01_165334_create_agendas_table.php
+++ database/migrations/2024_10_01_165334_create_agendas_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('agendas', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->number('date');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
