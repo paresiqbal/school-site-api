5cc140ca862c1b2e35e65242b4d3b85622b3647e
paresiqbal <thecrowdel@gmail.com>
2024-10-03 13:22:11 +0700

    Refactor AgendaController and add middleware for authentication

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -5,44 +5,41 @@ namespace App\Http\Controllers;
 use App\Models\Agenda;
 use App\Http\Requests\StoreAgendaRequest;
 use App\Http\Requests\UpdateAgendaRequest;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 
-class AgendaController extends Controller
+class AgendaController extends Controller implements HasMiddleware
 {
-    /**
-     * Display a listing of the resource.
-     */
+    public static function middleware(): array
+    {
+        return [
+            new Middleware('auth:sanctum', except: ['index', 'show'])
+        ];
+    }
+
     public function index()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(StoreAgendaRequest $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(Agenda $agenda)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(UpdateAgendaRequest $request, Agenda $agenda)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(Agenda $agenda)
     {
         //
