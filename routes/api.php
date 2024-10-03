311cc1e5e87fb469a40bde9df6ca8b3e08248771
paresiqbal <thecrowdel@gmail.com>
2024-10-03 14:02:36 +0700

    Refactor routes/api.php to add AgendaController to the API resources

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AgendaController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\NewsController;
 use Illuminate\Http\Request;
@@ -9,8 +10,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('news', NewsController::class);
-
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::apiResource('news', NewsController::class);
+Route::apiResource('agenda', AgendaController::class);
