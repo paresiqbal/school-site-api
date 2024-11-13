c0ded5fc21a578052ae37c1d085335091a383d23
paresiqbal <thecrowdel@gmail.com>
2024-11-13 13:57:22 +0700

    Add tags column to news table for improved categorization

--- database/migrations/2024_09_21_053308_create_news_table.php
+++ database/migrations/2024_09_21_053308_create_news_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->text('content');
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('image')->nullable();
+            $table->string('tags')->nullable();
             $table->timestamps();
         });
     }
