13e9116945f6e2df59ed5d081fb24bc895438af6
paresiqbal <thecrowdel@gmail.com>
2024-11-19 11:21:53 +0700

    Implement model event handling to delete associated images and detach tags on Achievement deletion

--- app/Models/Achievement.php
+++ app/Models/Achievement.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Achievement extends Model
 {
@@ -23,5 +24,26 @@ class Achievement extends Model
     public function images()
     {
         return $this->morphMany(ImageUpload::class, 'imageable');
+    }
+
+    /**
+     * Boot method to handle model events.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function ($announcement) {
+            // Delete associated images
+            foreach ($announcement->images as $image) {
+                if (Storage::disk('public')->exists($image->path)) {
+                    Storage::disk('public')->delete($image->path);
+                }
+                $image->delete();
+            }
+
+            // Detach all tags
+            $announcement->tags()->detach();
+        });
     }
 }
