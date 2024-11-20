25321bedf473d717101f70240fa9f1ec61289816
paresiqbal <thecrowdel@gmail.com>
2024-11-20 11:22:59 +0700

    Update CORS configuration to streamline allowed origins and methods

--- config/cors.php
+++ config/cors.php
@@ -1,32 +1,17 @@
 <?php
 
 return [
+    'paths' => ['api/*', 'image-upload'],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
+    'allowed_methods' => ['*'],
 
-    'paths' => ['api/*', 'image-upload'], // Ensure the right paths are set
+    'allowed_origins' => ['http://localhost:3000'],
 
-    'allowed_methods' => ['*'],  // Allow all HTTP methods
-
-    'allowed_origins' => ['http://localhost:3000'], // Your frontend URL
-
-    'allowed_headers' => ['*'],  // Allow all headers
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
     'supports_credentials' => false,
-
 ];
